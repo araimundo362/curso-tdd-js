7aa2b223fc80426974a5917810de0eff874d82d2
leilaybnz <leilanybanez@gmail.com>
2022-04-26 12:12:39 -0300

    chore: add prod code

--- fizzbuzz/src/fizzbuzz.js
+++ fizzbuzz/src/fizzbuzz.js
@@ -1,3 +1,44 @@
 export default () => {
-  return true;
+  const numbers = [];
+
+  for(let n = 1; n <= 100; n++) {
+
+    let transformedNumber = transform(n);
+
+    numbers.push(transformedNumber);
+
+  }
+  return numbers;
 }
+
+function transform(number) {
+
+  if (isBuzz(number) && isFizz(number)) {
+
+    return 'FizzBuzz';
+
+  } 
+  
+  if (isFizz(number)) {
+
+    return 'Fizz';
+
+  } 
+  
+  if (isBuzz(number)) {
+
+    return 'Buzz';
+
+  } 
+
+  return number;
+}
+
+function isFizz(n) {
+  return n % 3 == 0;
+}
+
+function isBuzz(n) {
+  return n % 5 == 0;
+}
+
